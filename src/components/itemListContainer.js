68ee360df9792c6c84e861bf6bab542221368f98
Jose <josemontilla1309@gmail.com>
2022-04-12 22:34:18 -0400

    Se agregan ids y retoques al itemListContainer

--- src/components/itemListContainer.js
+++ src/components/itemListContainer.js
@@ -2,35 +2,41 @@ import React from "react";
 
 
 const itemList = [
-    {name: "Zapatillas",
+    {id:1,
+      name: "Zapatillas",
      thumbnail: "https://www.cronista.com/files/image/295/295924/5ffe0a66110c3.webp?oe=jpg",
      brand: "Nike",
      size: "42",
      price: "150$"
   
     },
-    {name: "Sudadera",
+    {id:2,
+      name: "Sudadera",
      thumbnail: "https://www.innovasport.com/medias/sudadera-nike-sportswear-essential-is-BV4116-010-1.png?context=bWFzdGVyfGltYWdlc3w1NTUxM3xpbWFnZS9wbmd8aW1hZ2VzL2hmMS9oMWMvOTgzMDk3MTQ0MTE4Mi5wbmd8YTdiZjQ4MGZkOWE0ZmQ1NmRlOGJkMWI3NjEyMDIwMzQwZmEwMWJkOWM0OGJjMzE4N2FlMTg4NjY0MWU5MDRmNg",
      brand: "Nike",
      size: "L",
      price: "75$"
   
     },
-    {name: "Sudadera",
+    {id:3,
+      name: "Sudadera",
     thumbnail: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpadel.tienda%2Fcomhttps://padel.tienda/wp-content/uploads/2017/05/Sudadera-Adidas-Big-Logo-Azul_2021.jpg",
      brand: "Adidas",
      size: "L",
      price: "80$"
   
     },
-    {name: "Zapatillas",
+    {
+      id:4,
+      name: "Zapatillas",
     thumbnail: "https://e00-expansion.uecdn.es/assets/multimedia/imagenes/2021/02/16/16134939003134.jpg",
      brand: "Underarmour",
      size: "40",
      price: "130$"
   
     },
-    {name: "Chaqueta",
+    {id:5,
+      name: "Chaqueta",
     thumbnail: "https://chemasport.es/27707-thickbox_default/chaqueta-nike-sportswear-windrunner-negro.jpg",
      brand: "Nike",
      size: "S",
